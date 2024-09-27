0c67cf88731d7f96278ec9b3f8324a6ed9148d1e
root <root@DESKTOP-53DR3O3>
2024-09-27 05:19:28 -0500

    .

--- Tienda/store/urls.py
+++ Tienda/store/urls.py
@@ -6,8 +6,11 @@ app_name='users'
 
 urlpatterns=[
     re_path('^new/$',views.newProduct,name='newProduct'),
-    re_path('^view/<int:id>$',views.viewProduct,name='viewProduct'),
+    re_path('^view/$',views.viewProduct,name='viewProduct'),
+    re_path('^view/<int:id>$',views.detailProduct,name='viewDetailProduct'),
     re_path('^delete/<int:id>$',views.deleteProduct,name='deleteProduct'),
     re_path('^update/<int:id>$',views.updateProduct,name='updateProduct'),
     re_path('^whishlist/<int:id>$',views.wishlistProduct,name='wishlistProduct'),
+    re_path('^addToCart/<int:id>$',views.purchaseProduct,name='addToCart'),
+
 ]
--- Tienda/store/views.py
+++ Tienda/store/views.py
@@ -1,47 +1,35 @@
 from django import forms
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import gafas
-<<<<<<< HEAD
 from .forms import bodega,CreateProducts
 
-=======
-from users.forms import UserRegisterForm 
->>>>>>> aacf7767fbbfb486b47e8a83db329ee17a444d8e
-
-
-@login_required
-def index(request):
-    return render(request, 'index.html')
-
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('index')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
-
-#handles creation of new products 
-def newProduct(request):
-    return render(request, 'index.html')
+home='store:view'
 #handles creation of new products 
 def newProduct(View):
     def post(self,request):
         form=forms.CreateTask(request.POST)
         if form.is_valid():
+            instance=form.save(commit=False)
+            instance.save()
+            return redirect(home)
+        else:
+            form=CreateProducts()
+        return render(request,'store/newProduct.html',{'form':form})
 
 #handles view of individual products
-def viewProduct(view):
-    def get(self,**kwargs):
+def viewProduct(request):
+    def get(request):
+        Gafas=gafas
+        return render(request,'store/viewProduct.html',Gafas)
+
+def detailProduct(request,id):
+    def get(request):
+        product=gafas.objects.get(ItemId=id)
+        return render (request,'store/productDetail.html',{'product':product})
+
+def purchaseProduct(request):
+    def post(request):
         pass
 
 #handles deletion of new products
@@ -58,11 +46,6 @@ def updateProduct(view):
 def wishlistProduct():
     def post(self,request):
         pass
-<<<<<<< HEAD
 
 
 
-=======
-    
-    
->>>>>>> aacf7767fbbfb486b47e8a83db329ee17a444d8e
